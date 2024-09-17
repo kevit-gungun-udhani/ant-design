c1cdd592a3be00a374b3d96a16c95627f6ea7174
kevit-gungun-udhani <gungun.udhani@kevit.io>
2024-09-17 19:13:22 +0530

    Header/ almost

--- src/components/HeaderDropdown.jsx
+++ src/components/HeaderDropdown.jsx
@@ -1,34 +1,28 @@
-import { Dropdown, Space } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-
+import { Dropdown, Space, Statistic } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 const items = [
   {
-    label: <a href="https://www.antgroup.com">1st menu item</a>,
-    key: '0',
+    label: "1st menu item",
+    key: "0",
   },
   {
-    label: <a href="https://www.aliyun.com">2nd menu item</a>,
-    key: '1',
+    label: "item",
+    key: "1",
   },
   {
-    type: 'divider',
+    type: "divider",
   },
   {
-    label: '3rd menu item',
-    key: '3',
+    label: "3rd menu item",
+    key: "3",
   },
 ];
 
 export const HeaderDropdown = () => {
   return (
-    <Dropdown menu={{ items }} trigger={['click']}>
-      <a onClick={(e) => e.preventDefault()}>
-        <Space>
-          Click me
-          <DownOutlined />
-        </Space>
-      </a>
+    <Dropdown menu={{ items }} trigger={["hover"]}>
+      <Statistic title="organiation" value={'Workspace'} valueStyle={{fontSize: 'small', fontWeight: 'bold'}}/>
     </Dropdown>
   );
 };
--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,34 +1,43 @@
-import { ProductFilled } from "@ant-design/icons";
-import { Col, Layout, Row } from "antd";
+import { BellOutlined, ProductFilled, SaveOutlined } from "@ant-design/icons";
+import { Avatar, Col, Divider, Flex, Layout, Row, Space } from "antd";
 import { HeaderDropdown } from "./HeaderDropdown";
 const { Header } = Layout;
 
-// const items1 = ['1', '2', '3'].map((key) => ({
-//   key,
-//   label: `nav ${key}`,
-// }));
 
 export const SiteHeader = () => {
   return (
     <Header id="siteHeader">
-      <Row>
-        <Col>
-          <Row className="col-1-row">
-            <Col span={3}>
+      <Row className="row-1">
+        <Col className="main-col-1">
+          <Row>
+            <Col span={2}>
               <ProductFilled />
             </Col>
-            <Col span={2}>
+            <Col span={8}>
               <HeaderDropdown />
             </Col>
-            <Col span={3}>
+            <Col span={8}>
               <HeaderDropdown />
             </Col>
-            <Col span={2}>
+            <Col span={6}>
               <HeaderDropdown />
             </Col>
           </Row>
         </Col>
-        <Col></Col>
+        <Col className="col-2">
+          <Row>
+            <Col span={4}>
+              <SaveOutlined />
+            </Col>
+            <Col>
+              <BellOutlined />
+            </Col>
+            <Divider type="vertical" />
+            <Col span={5}>
+              <Avatar>MA</Avatar>
+            </Col>
+          </Row>
+        </Col>
       </Row>
     </Header>
   );
